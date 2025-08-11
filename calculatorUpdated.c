891b08c286327cf83b04ea3eeaf28261cafaee5f
rj-here <r.rishi.rj@gmail.com>
2025-08-11 12:23:42 -0400

    Upgrades
    Improving this program, hopefully completed by month-end

--- calculatorUpdated.c
+++ calculatorUpdated.c
@@ -26,6 +26,7 @@ int main() {
 printf("Welcome! This will be a simple calculator.\n");
 printf("This being a simple one, means it'll do simple addition, subtraction, multiplication and division.\n");
 printf("We'll calculate away!\n");
+while (strcmp(decision, "Yes") == 0 || strcmp(decision, "yes") == 0 || strcmp(decision, "YES") == 0) {//while the user wants to continue using the calculator
 
 printf("Before we proceed further, select one of the options, to decide which calculation you'd like to perform:\n");
 printf("1. Addition\n");
@@ -35,7 +36,6 @@ printf("4. Division\n");
 printf("5. Exponents\n");
 printf("6. Log [Base 10]\n");
 printf("7. Fractions\n");
-while (strcmp(decision, "Yes") == 0 || strcmp(decision, "yes") == 0 || strcmp(decision, "YES") == 0) {//while the user wants to continue using the calculator
 
 int choice;
 scanf("%d", &choice); //scanf takes user input.
@@ -147,9 +147,18 @@ else {
     printf("Sorry! Need a valid function!\n Try again.\n");
 }
 }
+printf("Would you like to see the history of previous calculations? (Either Type 'Yes' or 'No')\n");
+char viewOrNot[3];
+scanf("%s", viewOrNot); //taking the user's decision to view history or not
+if (strcmp(viewOrNot, "Yes") == 0 || strcmp(viewOrNot, "yes") == 0 || strcmp(viewOrNot, "YES") == 0) {
+    history(hist); //viewing the history
+}
+else {
+    printf("Okay! No history will be shown.\n");
+}
 printf("Do you desire to do another caluclation? (Either Type 'Yes' or 'No')\n");
 scanf("%s", decision); //taking the user's decision to continue using the calculator or not
-}
+}//while loop
 }
 /*
 The coming functions will perform: addition, subtraction, mutliplication, division [the essentials]
@@ -188,40 +197,45 @@ return log10(num); //get log of base 10 values
 }
 
 void fractions(int numerator1, int denominator1, int numerator2, int denominator2, int func) {
+if (denominator1 == 0 || denominator2 == 0) { //checking for zero denominators
+    printf("Error: Denominator cannot be zero.\n");
+    return;
+}
 //involving fractions
 int numerator, denominator; //declaring final
 if (func == 1) { //add
 if (denominator1 == denominator2) { //if same denominator
     numerator = numerator1 + numerator2;
-    printf("The fraction is: %d / %d", numerator, denominator1);
+    printf("The fraction is: %d / %d\n", numerator, denominator1);
 }
-else {
+else {//for different denominators
     numerator = (numerator1 * denominator2) + (numerator2 * denominator1);
     denominator = denominator1 * denominator2;
+    printf("The fraction is: %d / %d\n", numerator, denominator);
 }
 }
 
 else if (func == 2) { //subtract
     numerator = numerator1 * denominator2 - numerator2 * denominator1; //final numerator
     denominator = denominator1 * denominator2; //final denominator
-    printf("The fraction is %d / %d", numerator, denominator);
+    printf("The fraction is %d / %d\n", numerator, denominator);
 }
 
 else if (func == 3) { //multiply
 if (denominator1 == numerator2) { //if denominator of first is numerator of second
     numerator = numerator1; //final numerator
     denominator = denominator2; //final denominator
-    printf("The fraction is %d / %d", numerator, denominator);
+    printf("The fraction is %d / %d\n", numerator, denominator);
 }
 else if (denominator2 == numerator1) { //if denominator of second is numerator of first
     numerator = numerator2; //final numerator
     denominator = denominator1; //final denominator
-    printf("The fraction is %d / %d", numerator, denominator);
+    printf("The fraction is %d / %d\n", numerator, denominator);
 }
 else {
     numerator = numerator1 * numerator2; //final numerator
     denominator = denominator1 * denominator2; //final denominator
-    printf("The fraction is %d / %d", numerator, denominator);
+    printf("The fraction is %d / %d\n", numerator, denominator);
 }
 }
 
@@ -256,7 +270,7 @@ void addToHistory(char* expression, char[][MAX_LEN]) {
 }
 
 void history(char[][MAX_LEN]) {
-printf("Here are the previous calculations: ");
+printf("Here are the previous calculations:\n");
 for (int i = 0; i < MAX_LEN; i++) {
     if (hist[i][0] != '\0') { //string not empty
         printf("%s\n", hist[i]);
@@ -269,6 +283,6 @@ for (int i = 0; i < MAX_LEN; i++) {
 
 /*
 © 2025 Rishi
-Last updated: 5th August, 2025
+Last updated: 11th August, 2025
 */
 
