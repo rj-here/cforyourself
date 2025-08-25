9a908d1412df9a5f2736c61bc3de51c35bd390f3
rj-here <r.rishi.rj@gmail.com>
2025-08-25 14:59:50 -0400

    Clean up
    Removing unnecessary files, and appropriate simplification/renaming of Calculator.

--- calculator.c
+++ calculator.c
@@ -1,25 +1,32 @@
 /*
 This program will be a simple calculator, performing basic operations. I intend to add history as well, and also implementing multi-calculation (such as adding several numbers to understand finances or grades).
 This idea came to me as an AI suggestion.
-
-I have since moved subsequent code to calculatorUpdated.c, to add other functions, and allow this simple and functional version to remain.
 */
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <math.h>
+#define MAX_LEN 100 //The maximum length
+
+char hist[MAX_LEN][MAX_LEN]; // the most amount of expressions
+char decision[3] = "Yes"; //storing the user's decision to continue using the calculator or not (Yes or No)
+int histIndex = 0; // declaring the # of expressions done so far
 //Declaring the methods, so they can be used later!
 int add(int num1, int num2);
 int subtract(int num1, int num2);
 int multiply(int num1, int num2);
 int divide(int num1, int num2);
-int exponents(int base, int power);
-
+double exponents(double base, double power);
+double logOf10(int num);
+void fractions(int numerator1, int denominator1, int numerator2, int denominator2, int func);
+void addToHistory(char* expression, char[][MAX_LEN]);
+void history(char[][MAX_LEN]);
 
 int main() {
 printf("Welcome! This will be a simple calculator.\n");
 printf("This being a simple one, means it'll do simple addition, subtraction, multiplication and division.\n");
 printf("We'll calculate away!\n");
+while (strcmp(decision, "Yes") == 0 || strcmp(decision, "yes") == 0 || strcmp(decision, "YES") == 0) {//while the user wants to continue using the calculator
 
 printf("Before we proceed further, select one of the options, to decide which calculation you'd like to perform:\n");
 printf("1. Addition\n");
@@ -37,12 +44,15 @@ if (choice == 1) {//for addition
 printf("Adding away! Provide 2 numbers as input. Keep one space between them.\n");
 
 int num1, num2;
-
 scanf("%d %d", &num1, &num2);
 
 int sum = add(num1, num2);
 
 printf("The sum of the 2 numbers will be: %d\n", sum);
+
+char expression[MAX_LEN];
+sprintf(expression, "%d + %d = %d", num1, num2, sum); //formatting the expression, to add to history
+addToHistory(expression, hist); //Adding to history
 
 }
 
@@ -51,10 +61,12 @@ else if (choice == 2) {//for subtraction
 printf("Subtracting it'll be. Provide 2 numbers as input (it will be first number - second number). Keep one space between them.\n");
 
 int num1, num2;
-
 scanf("%d %d", &num1, &num2);
 
 int difference = subtract(num1, num2);
+char expression[MAX_LEN];
+sprintf(expression, "%d - %d = %d", num1, num2, difference); //formatting the expression, to add to history
+addToHistory(expression, hist); //Adding to history
 
 printf("The difference in the 2 numbers is: %d\n", difference);
 }
@@ -65,10 +77,12 @@ else if (choice == 3) {//for multiplication
 printf("Multiplying it is. Provide 2 numbers as input. Keep one space between them.\n");
 
 int num1, num2;
-
 scanf("%d %d", &num1, &num2);
 
 int product = multiply(num1, num2);
+char expression[MAX_LEN];
+sprintf(expression, "%d * %d = %d", num1, num2, product); //formatting the expression, to add to history
+addToHistory(expression, hist); //Adding to history
 
 printf("The product of the 2 numbers is %d\n", product);
 }
@@ -77,22 +91,27 @@ else if (choice == 4) {//for division
 printf("Dividing. Provide 2 numbers as input (it will be first number ÷ second number). Keep one space between them.\n");
 
 int num1, num2;
-
 scanf("%d %d", &num1, &num2);
 
 int dividedAnswer = divide(num1, num2);
-
-printf("Upon dividing the 2 numbers, the answer is: %d\n", dividedAnswer);
+char expression[MAX_LEN];
+sprintf(expression, "%d ÷ %d = %d", num1, num2, dividedAnswer); //formatting the expression, to add to history
+if (num2!=0) {
+    addToHistory(expression, hist); //Adding to history
+    printf("Upon dividing the 2 numbers, the answer is: %d\n", dividedAnswer);
+}
 }
 
 else if (choice == 5) {//for exponents.
 printf("Exponents here we come! Input 2 numbers, first the base, followed by the power. Keep one space between them.\n");
 
 int base, power;
-
 scanf("%d %d", &base, &power);
 
 int poweredBase = exponents(base, power);
+char expression[MAX_LEN];
+sprintf(expression, "%d ^ %d = %d\n", base, power, poweredBase); //formatting the expression, to add to history
+addToHistory(expression, hist); //Adding to history
 
 printf("%d to the power of %d is: %d", base, power, poweredBase);
 }
@@ -101,18 +120,47 @@ else if (choice == 6) {
 printf("Log base 10! Which number would you like to get the log base 10 value of?\n");
 
 int num;
-
 scanf("%d", &num);
+double logBase10 = logOf10(num); //get log of base 10 values
 
-printf("%d in log base 10 is: %d\n", num, log(num));
+char expression[MAX_LEN];
+sprintf(expression, "log10(%d) = %f", num, logBase10); //formatting the expression, to add to history
+addToHistory(expression, hist); //Adding to history
+
+printf("%d in log base 10 is: %f\n", num, logBase10);
 }
 
 else if (choice == 7) {//for fractions
 printf("Fractions! Provide 2 fractions as input, in this format: the numerator of the first, followed by the denominator of the first, then the numerator of the second, followed by the denominator of the second. Keep a space between each of them.\n");
 
-}
-}
+int numerator1, denominator1, numerator2, denominator2;
 
+scanf("%d %d %d %d", &numerator1, &denominator1, &numerator2, &denominator2);
+
+printf("Which function would you like to perform on the fractions?\n1. Add,\n2. Subtract,\n3. Multiply,\n4. Divide,\n5. Exponents\n");
+
+int func;
+scanf("%d", &func);
+if ((func >= 1) && (func <=5)) {
+    fractions(numerator1, denominator1, numerator2, denominator2, func);
+}
+else {
+    printf("Sorry! Need a valid function!\n Try again.\n");
+}
+}
+printf("Would you like to see the history of previous calculations? (Either Type 'Yes' or 'No')\n");
+char viewOrNot[3];
+scanf("%s", viewOrNot); //taking the user's decision to view history or not
+if (strcmp(viewOrNot, "Yes") == 0 || strcmp(viewOrNot, "yes") == 0 || strcmp(viewOrNot, "YES") == 0) {
+    history(hist); //viewing the history
+}
+else {
+    printf("Okay! No history will be shown.\n");
+}
+printf("Do you desire to do another caluclation? (Either Type 'Yes' or 'No')\n");
+scanf("%s", decision); //taking the user's decision to continue using the calculator or not
+}//while loop
+}
 /*
 The coming functions will perform: addition, subtraction, mutliplication, division [the essentials]
 We will take 2 numbers in each case, then return the according result.
@@ -135,25 +183,128 @@ return product;
 
 int divide(int num1, int num2) {
 if (num2 == 0) {
-    printf("Error: Division by zero is not allowed.\n");
-    return 0; // or another error code
+    printf("Error: Division by zero is not allowed.\nThis will not be added to history!\n");
+    return 0; //Not a number
 }
 int answer = num1 / num2; //dividing
 return answer;
 }
-/*
-Extras
-*/
-int exponents(int base, int power) {
-int value = 1;
-for (int i = 0; i < power; i++) {
-    value = value * base;
+double exponents(double base, double power) {
+return pow(base, power);
 }
-return value; //get x^n
+
+double logOf10(int num) {
+return log10(num); //get log of base 10 values
+}
+
+void fractions(int numerator1, int denominator1, int numerator2, int denominator2, int func) {
+if (denominator1 == 0 || denominator2 == 0) { //checking for zero denominators
+    printf("Error: Denominator cannot be zero.\n");
+    return;
+}
+//involving fractions
+int numerator, denominator; //declaring final
+if (func == 1) { //add
+if (denominator1 == denominator2) { //if same denominator
+    numerator = numerator1 + numerator2;
+    denominator = denominator1; //final denominator
+    printf("The fraction is: %d / %d\n", numerator, denominator);
+    
+}
+else {//for different denominators
+    numerator = (numerator1 * denominator2) + (numerator2 * denominator1);
+    denominator = denominator1 * denominator2;
+    printf("The fraction is: %d / %d\n", numerator, denominator);
+}
+    char expression[MAX_LEN];
+    sprintf(expression, "(%d / %d) + (%d / %d) = (%d / %d)", numerator1, denominator1, numerator2, denominator2, numerator, denominator); //formatting the expression, to add to history
+    addToHistory(expression, hist); //Adding to history
+}
+
+else if (func == 2) { //subtract
+    numerator = numerator1 * denominator2 - numerator2 * denominator1; //final numerator
+    denominator = denominator1 * denominator2; //final denominator
+    printf("The fraction is %d / %d\n", numerator, denominator);
+    char expression[MAX_LEN];
+    sprintf(expression, "(%d / %d) - (%d / %d) = (%d / %d)", numerator1, denominator1, numerator2, denominator2, numerator, denominator); //formatting the expression, to add to history
+    addToHistory(expression, hist); //Adding to history
+}
+
+else if (func == 3) { //multiply
+if (denominator1 == numerator2) { //if denominator of first is numerator of second
+    numerator = numerator1; //final numerator
+    denominator = denominator2; //final denominator
+    printf("The fraction is %d / %d\n", numerator, denominator);
+    
+}
+else if (denominator2 == numerator1) { //if denominator of second is numerator of first
+    numerator = numerator2; //final numerator
+    denominator = denominator1; //final denominator
+    printf("The fraction is %d / %d\n", numerator, denominator);
+}
+else {
+    numerator = numerator1 * numerator2; //final numerator
+    denominator = denominator1 * denominator2; //final denominator
+    printf("The fraction is %d / %d\n", numerator, denominator);
+}
+    char expression[MAX_LEN];
+    sprintf(expression, "(%d / %d) * (%d / %d) = (%d / %d)", numerator1, denominator1, numerator2, denominator2, numerator, denominator); //formatting the expression, to add to history
+    addToHistory(expression, hist); //Adding to history
+}
+
+else if (func == 4) { //divide; dividing a fraction by another is multiplying by the reciprocal
+    numerator = numerator1 * denominator2; //final numerator
+    denominator = denominator1 * numerator2; //final denominator
+    if (denominator == 0) {
+        printf("Error: Division by zero is not allowed.\n");
+        return;
+    }
+    printf("The fraction is %d / %d\n", numerator, denominator);
+    char expression[MAX_LEN];
+    sprintf(expression, "(%d / %d) ÷ (%d / %d) = (%d / %d)", numerator1, denominator1, numerator2, denominator2, numerator, denominator); //formatting the expression, to add to history
+    addToHistory(expression, hist); //Adding to history
+}
+
+else if (func == 5) { //exponents
+    double base = (double) numerator1 / denominator1; //final base
+    double power = (double) numerator2 / denominator2; //final power
+    //All these values are being done, to get an expression of the form (a/b) ^ (c/d), to print and store.
+
+    double poweredFraction = exponents(base, power); //Extracting the value as is. This is the final value of the fraction raised to the power.
+    printf("The fraction is: %.6f (up to 6 decimal places)\n", poweredFraction); //Printing the fraction
+    char expression[MAX_LEN];
+    sprintf(expression, "(%d / %d) ^ (%d / %d) = %.6f", numerator1, denominator1, numerator2, denominator2, poweredFraction); //formatting the expression, to add to history
+    addToHistory(expression, hist); //Adding to history
+}
+
+}
+
+void addToHistory(char* expression, char[][MAX_LEN]) {
+    for (int i = 0; i < MAX_LEN; i++) {
+        if (hist[i][0] == '\0') { //finding the first vacant slot
+              strcpy(hist[i], expression); //bringing in the expression to the history
+              break; //this stops subseqeunt additions of the same expression
+        }
+        else {
+            continue; //just go to the next one
+        }
+    }
+}
+
+void history(char[][MAX_LEN]) {
+printf("Here are the previous calculations:\n");
+for (int i = 0; i < MAX_LEN; i++) {
+    if (hist[i][0] != '\0') { //string not empty
+        printf("%s\n", hist[i]);
+    }
+    else {
+        break; //if empty, stop
+    }
+}
 }
 
 /*
 © 2025 Rishi
-Last updated: 10th July, 2025
+Last updated: 18th August, 2025
 */
 
